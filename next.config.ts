287a2ec3c4e3f12e92f09fcabc26cfac7c09cfff
Ajmal-Padalath <167340336+Ajmal-Padalath@users.noreply.github.com>
2026-07-29 17:05:39 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "*.unsplash.com" },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "4mb",
-    },
-  },
+const nextConfig = {
+  output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
